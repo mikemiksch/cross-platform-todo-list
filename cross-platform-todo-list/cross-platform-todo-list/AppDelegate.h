24febd24b915a1a8fb608fb6b5a88eaa00fc5296
mikemiksch <mikemiksch@gmail.com>
2017-05-08 10:08:45 -0700

    Helps if I make the project in the write language

---
+++ cross-platform-todo-list/cross-platform-todo-list/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cross-platform-todo-list
+//
+//  Created by Mike Miksch on 5/8/17.
+//  Copyright © 2017 Mike Miksch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
