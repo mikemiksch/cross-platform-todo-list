1ec6c2e75574e730aed964afcb3b77cd68320b75
mikemiksch <mikemiksch@gmail.com>
2017-05-09 12:17:39 -0700

    Lecture notes completed.

---
+++ cross-platform-todo-list/TV-todo-list/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TV-todo-list
+//
+//  Created by Mike Miksch on 5/9/17.
+//  Copyright © 2017 Mike Miksch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cross-platform-todo-list/WatchKit-todo-list Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  WatchKit-todo-list Extension
+//
+//  Created by Mike Miksch on 5/9/17.
+//  Copyright © 2017 Mike Miksch. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
