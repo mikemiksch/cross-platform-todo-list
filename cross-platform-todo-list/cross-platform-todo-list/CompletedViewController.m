99ee3eb0eeb89d73a6cd43393102f09631f9c6db
mikemiksch <mikemiksch@gmail.com>
2017-05-10 13:29:07 -0700

    Pods disappeared for some reason, restored them. Built out completed task view.

--- cross-platform-todo-list/cross-platform-todo-list/CompletedViewController.m
+++ cross-platform-todo-list/cross-platform-todo-list/CompletedViewController.m
@@ -7,8 +7,19 @@
 //
 
 #import "CompletedViewController.h"
+#import "LoginViewController.h"
+#import "TodoItem.h"
 
-@interface CompletedViewController ()
+@import Firebase;
+
+@interface CompletedViewController () <UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *completedTableView;
+
+@property (strong, nonatomic) FIRDatabaseReference *userReference;
+@property (strong, nonatomic) FIRUser *currentUser;
+@property (nonatomic) FIRDatabaseHandle allTodosHandler;
+@property (strong, nonatomic) NSMutableArray *allTodos;
+
 
 @end
 
@@ -16,14 +27,65 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.completedTableView.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self setupFirebase];
+    [self startMonitoringTodoUpdates];
 }
 
 
+-(void)setupFirebase {
+    FIRDatabaseReference *databaseReference = [[FIRDatabase database]reference];
+    self.currentUser = [[FIRAuth auth] currentUser];
+    self.userReference = [[databaseReference child:@"users"] child:self.currentUser.uid];
+    
+    NSLog(@"User reference: %@", self.userReference);
+}
+
+
+-(void)startMonitoringTodoUpdates {
+    self.allTodosHandler = [[self.userReference child:@"todos"] observeEventType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
+        
+        self.allTodos = [[NSMutableArray alloc]init];
+        
+        for (FIRDataSnapshot *child in snapshot.children) {
+            NSDictionary *todoData = child.value;
+            NSString *todoTitle = todoData[@"title"];
+            NSString *todoContent = todoData[@"content"];
+            NSNumber *todoCompleted = todoData[@"completed"];
+            
+            if (todoCompleted.integerValue == 1) {
+                TodoItem *currentTodo = [[TodoItem alloc]init];
+                currentTodo.title = todoTitle;
+                currentTodo.content = todoContent;
+                [self.allTodos addObject:currentTodo];
+            }
+            
+        }
+        [self.completedTableView reloadData];
+    }];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.allTodos.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    TodoItem *todo = [self.allTodos objectAtIndex:indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", todo.title];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%@", todo.content];
+    return cell;
+}
 
 @end
+
+
+
+
+
+
+
